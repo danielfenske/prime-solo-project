373349f96d0393dd1a369f29bd0d995aafbad362
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-03-15 11:21:54 -0500

    Setup and tested /workout GET router to grab one workout template from database reflected of user's plan and day of the plan they are on

--- server/routes/workout.router.js
+++ server/routes/workout.router.js
@@ -2,28 +2,76 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 const dummyExerciseData = require('../modules/dummyExerciseData');
+const phaseData = require('../modules/phaseData');
 
-let preferences = [];
+// 'workoutTemplates' represents all templates that 
+// correspond with the user's days_per_week value
+// // 'selectedTemplate' represents the selected template 
+// // that corresponds with the day the user is on
+let workoutTemplates;
+let selectedTemplate;
 
 // #region ==== GET ROUTES ====
 // get user preferences 
 router.get('/preferences', (req, res) => {
-  let id = 3
-  
+  let id = 3; // req.params.id
+
   let queryText = `SELECT * FROM "user_preferences" WHERE id = $1;`
 
   pool.query(queryText, [id])
     .then((result) => {
       res.send(result.rows);
 
-      preferences = result.rows;
+      let preferences = result.rows;
 
       console.log('preferences', preferences);
+      console.log('phaseData', phaseData);
+
     })
     .catch((error) => {
       res.sendStatus(500);
     })
 });
+
+// get workout request first starts with grabbing all eligible
+// templates from the database and selecting template one that corresponds
+// to the day the user is on in their week (ex: working out twice a week, on second day)
+router.get('/', (req, res) => {
+  let phase = 'endurance'; // req.body.phase
+  let days_per_week = 4; // req.body.days_per_week
+  let day = 3; // req.body.day
+
+  let queryText = `SELECT * FROM "full_body_workouts" WHERE "days_per_week" = $1;`;
+
+  pool.query(queryText, [days_per_week])
+    .then((result) => {
+      res.send(result.rows);
+
+      workoutTemplates = result.rows;
+
+      switch (day) {
+        case 1:
+          selectedTemplate = workoutTemplates[0];
+          break;
+        case 2:
+          selectedTemplate = workoutTemplates[1];
+          break;
+        case 3:
+          selectedTemplate = workoutTemplates[2]
+          break;
+        case 4:
+          selectedTemplate = workoutTemplates[3];
+          break;
+        default:
+          console.log('Unable to assign workout');
+      }
+      console.log('User workout is', selectedTemplate);
+      setDailyWorkout(selectedTemplate);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+    })
+})
 // #endregion ====
 
 /**
