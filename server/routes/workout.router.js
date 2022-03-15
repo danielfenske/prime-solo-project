6cc288be8ed84a2dcb5e9b9c77ccfc29ade977ea
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-03-15 09:42:36 -0500

    Setup GET route on server to grab user preferences from database and tested in Postman; also created phaseData.js module

--- server/routes/workout.router.js
+++ server/routes/workout.router.js
@@ -3,13 +3,28 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const dummyExerciseData = require('../modules/dummyExerciseData');
 
-/**
- * GET route template
- */
-router.get('/', (req, res) => {
-  // GET route code here
-  console.log('dummyExerciseData in template.router.js', dummyExerciseData);
+let preferences = [];
+
+// #region ==== GET ROUTES ====
+// get user preferences 
+router.get('/preferences', (req, res) => {
+  let id = 3
+  
+  let queryText = `SELECT * FROM "user_preferences" WHERE id = $1;`
+
+  pool.query(queryText, [id])
+    .then((result) => {
+      res.send(result.rows);
+
+      preferences = result.rows;
+
+      console.log('preferences', preferences);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+    })
 });
+// #endregion ====
 
 /**
  * POST route template
