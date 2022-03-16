cffc7e6c8c94b2fef8665f1ac2fd52642256d56f
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-03-16 12:02:03 -0500

    Established async and await in /workout route to control and easily organize flow of queries and functions within router

--- server/routes/workout.router.js
+++ server/routes/workout.router.js
@@ -3,6 +3,7 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const dummyExerciseData = require('../modules/dummyExerciseData');
 const phaseData = require('../modules/phaseData');
+const { default: axios } = require('axios');
 
 // 'dailyWorkout' is the workout that will be sent to the user
 // after all filters have been applied
@@ -31,40 +32,63 @@ router.get('/preferences/:id', (req, res) => {
 // get workout request first starts with grabbing all eligible
 // templates from the database and selecting one template that corresponds
 // to the day the user is on in their week (ex: working out twice a week, on second day)
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const id = req.user.id
+  
+  if (req.isAuthenticated()) {
+    // pulls user's days_per_week integer in database to determine which templates they are eligible for
+    const daysQuery = await pool.query(`SELECT "user_preferences"."days_per_week" FROM "user_preferences" WHERE id=$1;`, [id])
+    const daysPerWeek = daysQuery.rows[0].days_per_week;
+    console.log('daysPerWeek', daysPerWeek);
 
-  let phase = 'endurance'; // req.body.phase
-  let days_per_week = 4; //req.body.days_per_week
-  let day = 4; //req.body.day
-  console.log('req.body.equipment', req.body.equipment);
-  let equipment = req.body.equipment;
+    // selects all eligible templates based off of user's daysPerWeek value
+    const templatesQuery = await pool.query(`SELECT * FROM "full_body_workouts" WHERE "days_per_week" = $1 ORDER BY "id";`, [daysPerWeek])
+    const workoutTemplates = templatesQuery.rows;
+    console.log('workoutTemplates', workoutTemplates);  
 
-  let queryText = `SELECT * FROM "full_body_workouts" WHERE "days_per_week" = $1 ORDER BY "id";`;
-
-  pool.query(queryText, [days_per_week])
-    .then((result) => {
-
-      // 'workoutTemplates' represents all templates that 
-      // correspond with the user's days_per_week value
-      // 'selectedTemplate' represents the selected template 
-      // that corresponds with the day the user is on
-      let workoutTemplates = result.rows;
-      let selectedTemplate = selectUserTemplate(day, workoutTemplates);
-      console.log('selectedTemplate', selectedTemplate);
-
-      // after selected template and array of exercises taken from 
-      // pulled from exerciseDB is sent to groupExercises function 
-      // to find matches between the two groups
-      groupExercises(selectedTemplate, dummyExerciseData, equipment);
-      res.send(dailyWorkout);
-
-      dailyWorkout = [];
+    // grabs exercise data from ExerciseDB database
+    const exerciseAPIQuery = await axios.get(`https://exercisedb.p.rapidapi.com/exercises`, {
+      headers: {
+        'x-rapidapi-host': 'exercisedb.p.rapidapi.com',
+        'x-rapidapi-key': `${process.env.EXERCISE_DB_API_KEY}`
+      }
     })
-    .catch((error) => {
-      console.log('error', error);
+    const allExercises = exerciseAPIQuery;
+    
+  }
 
-      res.sendStatus(500);
-    })
+  // let phase = 'endurance'; // req.body.phase
+  // let days_per_week = 4; //req.body.days_per_week
+  // let day = 4; //req.body.day
+  // console.log('req.body.equipment', req.body.equipment);
+  // let equipment = req.body.equipment;
+
+  // let queryText = `SELECT * FROM "full_body_workouts" WHERE "days_per_week" = $1 ORDER BY "id";`;
+
+  // pool.query(queryText, [days_per_week])
+  //   .then((result) => {
+
+  //     // 'workoutTemplates' represents all templates that 
+  //     // correspond with the user's days_per_week value
+  //     // 'selectedTemplate' represents the selected template 
+  //     // that corresponds with the day the user is on
+  //     let workoutTemplates = result.rows;
+  //     let selectedTemplate = selectUserTemplate(day, workoutTemplates);
+
+  //     // after selected template and array of exercises taken from 
+  //     // pulled from exerciseDB is sent to groupExercises function 
+  //     // to find matches between the two groups
+  //     let dailyWorkout = buildWorkout(dummyExerciseData, selectedTemplate);
+  //     // groupExercises(selectedTemplate, dummyExerciseData, equipment);
+  //     // res.send(dailyWorkout);
+
+  //     dailyWorkout = [];
+  //   })
+  //   .catch((error) => {
+  //     console.log('error', error);
+
+  //     res.sendStatus(500);
+  //   })
 })
 
 
@@ -116,6 +140,48 @@ let selectUserTemplate = (day, workoutTemplates) => {
   return selectedTemplate;
 }
 
+let buildWorkout = (exerciseArray) => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let groupExercises = (obj, arrOne, arrTwo) => {
 
   const exercises = Object.values(obj);
@@ -132,52 +198,7 @@ let groupExercises = (obj, arrOne, arrTwo) => {
   let e_tens = [];
   let e_elevens = [];
 
-
-  // for (let i = 0; i < arrOne.length; i++) {
-
-  //   for (let j = 0; j < arrTwo.length; j++) {
-  //     if (arrOne[i].equipment === arrTwo[j]) {
-  //       switch (arrOne[i].target) {
-  //         case exercises[2]:
-  //           e_ones.push(arrOne[i]);
-  //           break;
-  //         case exercises[3]:
-  //           e_twos.push(arrOne[i]);
-  //           break;
-  //         case exercises[4]:
-  //           e_threes.push(arrOne[i]);
-  //           break;
-  //         case exercises[5]:
-  //           e_fours.push(arrOne[i]);
-  //           break;
-  //         case exercises[6]:           
-  //           e_fives.push(arrOne[i]);
-  //           break;
-  //         case exercises[7]:
-  //           e_sixes.push(arrOne[i]);
-  //           break;
-  //         case exercises[8]:
-  //           e_sevens.push(arrOne[i]);
-  //           break;
-  //         case exercises[9]:
-  //           e_eights.push(arrOne[i]);
-  //           break;
-  //         case exercises[10]:
-  //           e_nines.push(arrOne[i]);
-  //           break;
-  //         case exercises[11]:
-  //           e_tens.push(arrOne[i]);
-  //           break;
-  //         case exercises[12]:
-  //           e_elevens.push(arrOne[i]);
-  //           break;
-  //       }
-  //     }
-  //   }
-  // }
-
   for (let i = 0; i < arrOne.length; i++) {
-
     for (let j = 0; j < arrTwo.length; j++) {
       if (arrOne[i].equipment === arrTwo[j]) {
         switch (arrOne[i].target) {
