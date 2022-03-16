a1bb96e091f4a76d5430d51538bbe6a037a43a8a
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-03-15 20:48:00 -0500

    Called each array of exercises to 'getRandomExercises' function, which selects one from each group and adds it to the dailyWorkout array

--- server/routes/workout.router.js
+++ server/routes/workout.router.js
@@ -6,19 +6,14 @@ const phaseData = require('../modules/phaseData');
 
 // 'dailyWorkout' is the workout that will be sent to the user
 // after all filters have been applied
+let dailyWorkout = [];
 
-// 'workoutTemplates' represents all templates that 
-// correspond with the user's days_per_week value
-// // 'selectedTemplate' represents the selected template 
-// // that corresponds with the day the user is on
-let workoutTemplates;
-let selectedTemplate;
 let eligibleExercises = [];
 
 // #region ==== GET ROUTES ====
 // get user preferences 
-router.get('/preferences', (req, res) => {
-  let id = 3; // req.params.id
+router.get('/preferences/:id', (req, res) => {
+  let id = req.params.id;
 
   let queryText = `SELECT * FROM "user_preferences" WHERE id = $1;`
 
@@ -41,15 +36,23 @@ router.get('/preferences', (req, res) => {
 // templates from the database and selecting one template that corresponds
 // to the day the user is on in their week (ex: working out twice a week, on second day)
 router.get('/', (req, res) => {
+  console.log('req.body', req.body);
+
   let phase = 'endurance'; // req.body.phase
-  let days_per_week = 1; // req.body.days_per_week
-  let day = 1; // req.body.day
+  let days_per_week = 2; //req.body.days_per_week
+  let day = 1; //req.body.day
 
   let queryText = `SELECT * FROM "full_body_workouts" WHERE "days_per_week" = $1 ORDER BY "id";`;
 
   pool.query(queryText, [days_per_week])
     .then((result) => {
-      workoutTemplates = result.rows;
+
+      // 'workoutTemplates' represents all templates that 
+      // correspond with the user's days_per_week value
+        // 'selectedTemplate' represents the selected template 
+        // that corresponds with the day the user is on
+      let workoutTemplates = result.rows;
+      let selectedTemplate;
 
       switch (day) {
         case 1:
@@ -68,8 +71,8 @@ router.get('/', (req, res) => {
           console.log('Unable to assign workout');
       }
 
-      selectExercises(selectedTemplate, dummyExerciseData);
-      res.send(eligibleExercises);
+      groupExercises(selectedTemplate, dummyExerciseData);
+      res.send(dailyWorkout);
     })
     .catch((error) => {
       console.log('error', error);
@@ -79,7 +82,7 @@ router.get('/', (req, res) => {
 })
 // #endregion ====
 
-function selectExercises(obj, array) {
+function groupExercises(obj, array) {
 
   const exercises = Object.values(obj);
 
@@ -106,70 +109,102 @@ function selectExercises(obj, array) {
   //   }
   // }
 
-  for (let i = 0; i < dummyExerciseData.length; i++) {
-    switch (dummyExerciseData[i].target) {
+  for (let i = 0; i < array.length; i++) {
+    switch (array[i].target) {
       case exercises[2]:
-        e_ones.push(dummyExerciseData[i]);
+        e_ones.push(array[i]);
         break;
       case exercises[3]:
-        e_twos.push(dummyExerciseData[i]);
+        e_twos.push(array[i]);
         break;
       case exercises[4]:
-        e_threes.push(dummyExerciseData[i]);
+        e_threes.push(array[i]);
         break;
       case exercises[5]:
-        e_fours.push(dummyExerciseData[i]);
+        e_fours.push(array[i]);
         break;
       case exercises[6]:
-        e_fives.push(dummyExerciseData[i]);
+        e_fives.push(array[i]);
         break;
       case exercises[7]:
-        e_sixes.push(dummyExerciseData[i]);
+        e_sixes.push(array[i]);
         break;
       case exercises[8]:
-        e_sevens.push(dummyExerciseData[i]);
+        e_sevens.push(array[i]);
         break;
       case exercises[9]:
-        e_eights.push(dummyExerciseData[i]);
+        e_eights.push(array[i]);
         break;
       case exercises[10]:
-        e_nines.push(dummyExerciseData[i]);
+        e_nines.push(array[i]);
         break;
       case exercises[11]:
-        e_tens.push(dummyExerciseData[i]);
+        e_tens.push(array[i]);
         break;
       case exercises[12]:
-        e_elevens.push(dummyExerciseData[i]);
+        e_elevens.push(array[i]);
         break;
     }
   }
 
+  // eligibleExercises = [
+  //   e_ones,
+  //   e_twos,
+  //   e_threes,
+  //   e_fours,
+  //   e_fives,
+  //   e_sixes,
+  //   e_sevens,
+  //   e_eights,
+  //   e_nines,
+  //   e_tens,
+  //   e_elevens,
+  // ]
+
+  getRandomExercises(e_ones);
+  getRandomExercises(e_twos);
+  getRandomExercises(e_threes);
+  getRandomExercises(e_fours);
+  getRandomExercises(e_fives);
+  getRandomExercises(e_sixes);
+  getRandomExercises(e_sevens);
+  getRandomExercises(e_eights);
+  getRandomExercises(e_nines);
+  getRandomExercises(e_tens);
+  getRandomExercises(e_elevens);
+  // ==== #region LOGS/TESTING ====
+  // notes: if multiple values exist, all
+  // subsequent arrays will not have exercises
+  // pushed to them
   // console.log('e_ones', e_ones);
   // console.log('e_twos', e_twos);
-  // console.log('e_threes ', e_threes ); // look over
+  // console.log('e_threes ', e_threes );
   // console.log('e_fours', e_fours);
   // console.log('e_fives', e_fives);
   // console.log('e_sixes', e_sixes);
   // console.log('e_sevens ', e_sevens );
-  console.log('e_eights ', e_eights );
+  // console.log('e_eights ', e_eights );
   // console.log('e_nines', e_nines);
   // console.log('e_tens ', e_tens );
   // console.log('e_elevens', e_elevens);
+  // === #endregion
 }
 
 // 'getRandomExercise' randomly selects on exercise from 
 // the given array and returns it as an object to be added
 // to the workout for that day
-function getRandomExercise(arr) {
+function getRandomExercises(arr) {
+
   // get random index value
   const randomIndex = Math.floor(Math.random() * arr.length);
 
   // get random exercise
   const exercise = arr[randomIndex];
 
+  // push random exercise to dailyWorkout for user
   dailyWorkout.push(exercise);
 
-  return item;
+  return;
 }
 
 /**
