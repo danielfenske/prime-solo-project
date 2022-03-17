e85f4080e5e68e3f6ada763b52407a49d90a4635
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-03-17 13:39:57 -0500

    Reconfigured client to direct users to HomeView component upon login

--- src/components/HomeView/HomeView.jsx
+++ src/components/HomeView/HomeView.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 
-function UserPage() {
+
+
+function HomeView() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
+    <div className="appContainer">
+      {/* <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      <LogOutButton className="btn" /> */}
+
+      {/* QuickLift HomeView */}
+      <h1 className="headerText">Welcome back, {user.username}!</h1>
+
+      <FormHelperText>Required</FormHelperText>
     </div>
   );
 }
 
 // this allows us to use <App /> in index.js
-export default UserPage;
+export default HomeView;
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -8,8 +8,10 @@ function LoginPage() {
   return (
     <div>
       <LoginForm />
-
-      <center>
+      <div>
+        <p>
+          new user?
+        </p>
         <button
           type="button"
           className="btn btn_asLink"
@@ -17,9 +19,9 @@ function LoginPage() {
             history.push('/registration');
           }}
         >
-          Register
+          create account
         </button>
-      </center>
+      </div>
     </div>
   );
 }
