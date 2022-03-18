ffa4cd986a174cdebad94ff2e856b442caa12547
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-03-17 22:34:02 -0500

    Added Equipment component to ProfileView to render all pieces of equipment on the DOM

--- src/components/ProfileView/ProfileView.jsx
+++ src/components/ProfileView/ProfileView.jsx
@@ -3,6 +3,9 @@ import React, { useEffect } from 'react';
 // import redux dependencies 
 import { useSelector, useDispatch } from 'react-redux';
 
+// import equipment component
+import Equipment from '../Equipment/Equipment';
+
 function ProfileView() {
 
     // bring in state stored in redux for all data related to profile
@@ -14,13 +17,36 @@ function ProfileView() {
 
     // grab user profile information on page load
     useEffect(() => {
-        dispatch({type:'FETCH_USER_PROFILE'});
+        dispatch({ type: 'FETCH_USER_PROFILE' });
     }, []);
 
     return (
         <div className="appContainer">
             <h1 className="headerText">{userPreferences.name}</h1>
-            
+            <h1 className="subHeaderText">{userPreferences.days_per_week}</h1>
+            <p>{userPreferences.weight}</p>
+            <p>{userPreferences.height}</p>
+            <p>{userPreferences.age}</p>
+
+            <div className="maxesContainer">
+                <p>Bench: 300</p>
+                <p>Squat: 225</p>
+                <p>Pull ups: 20</p>
+            </div>
+
+            <div className="equipmentContainer">
+                <h1 className="subHeaderText">EQUIPMENT</h1>
+                {
+                    userEquipment && (userEquipment.map((equipment, index) => {
+                        return (
+                            <Equipment
+                                key={index}
+                                equipment={equipment}
+                            />
+                        )
+                    }))
+                }
+            </div>
         </div>
     )
 }
