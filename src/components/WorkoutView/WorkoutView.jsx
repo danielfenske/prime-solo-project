c41a2f1802fa7793f82acc7e7409f89c4d0d58e2
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-03-17 20:22:18 -0500

    Setup basic layout for WorkoutView and mapped all exercises to it using the Exercise component

--- src/components/WorkoutView/WorkoutView.jsx
+++ src/components/WorkoutView/WorkoutView.jsx
@@ -3,10 +3,27 @@ import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
 
+// import Exercise component
+import Exercise from '../Exercise/Exercise';
+
+// import CSS
+import './WorkoutView.css';
+
 function WorkoutView() {
+
+    const dailyWorkout = useSelector((store) => store.dailyWorkout);
+
     return (
         <div className="appContainer">
             <h1 className="headerText">LET'S GET IT</h1>
+            {dailyWorkout ? (dailyWorkout.map((exercise) => {
+                return (
+                    <Exercise 
+                        key={exercise.id}
+                        exercise={exercise}
+                    />
+                );
+            })) : <p>Loading</p>}
         </div>
     )
 }
