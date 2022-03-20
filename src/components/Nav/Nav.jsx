b94f0e8d80faf6b1cc4e3274c2fc0bf57a598ccf
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-03-20 17:40:01 -0500

    Changed favicon; imported SASS; stylized NavBar and included MUI icons

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,45 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
+import './Nav.scss';
 import { useSelector } from 'react-redux';
 
+// icon imports
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import StackedLineChartOutlinedIcon from '@mui/icons-material/StackedLineChartOutlined';
+import PersonIcon from '@mui/icons-material/Person';
+
 function Nav() {
-  const user = useSelector((store) => store.user);
 
   return (
-    <div className="nav">
+    <div className="navBar">
       <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+        <img src="../public/images/login-background.jpg"/>
       </Link>
-      <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
 
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
+      <Link className="navLink" to="/workout">
+        <FitnessCenterIcon fontSize="medium" />
+      </Link>
 
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
+      <Link className="navLink" to="/maxes">
+        <StackedLineChartOutlinedIcon fontsize="large" />
+      </Link>
 
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-      </div>
+      <Link className="navLink" to="/profile">
+        <PersonIcon fontsize="large" />
+      </Link>
     </div>
   );
 }
